540a1eedc5fa555146f71e0ad689e7b3ef680796
brunojustino <8812217+brunojustino@users.noreply.github.com>
2024-06-21 23:32:12 -0300

    Initial boilerplate repo

---
+++ README.md
@@ -0,0 +1,40 @@
+# my-app
+
+An Electron application with React and TypeScript
+
+## Created with:
+```bash
+npm create @quick-start/electron my-app -- --template react-ts
+```
+And added tailwind.
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint) + [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
+
+## Project Setup
+
+### Install
+
+```bash
+$ npm install
+```
+
+### Development
+
+```bash
+$ npm run dev
+```
+
+### Build
+
+```bash
+# For windows
+$ npm run build:win
+
+# For macOS
+$ npm run build:mac
+
+# For Linux
+$ npm run build:linux
+```
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,27 @@
+import Versions from './components/Versions'
+// import electronLogo from './assets/electron.svg'
+
+function App(): JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <div className="text-3xl text-cyan-700">Test</div>
+      <div className="actions">
+        <div className="action">
+          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+            Documentation
+          </a>
+        </div>
+        <div className="action">
+          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+            Send IPC
+          </a>
+        </div>
+      </div>
+      <Versions></Versions>
+    </>
+  )
+}
+
+export default App
